4fcc297d94ce8f575c32b968f6c941dcbdc5a9d4
Mateus-Marques-B <141762674+Mateus-Marques-B@users.noreply.github.com>
2024-09-13 11:11:52 -0300

    Update script.js

--- palindrome/script.js
+++ palindrome/script.js
@@ -1,18 +1,15 @@
 const btn = document.getElementById("check-btn");
-const txt = document.getElementById("text-input"); //value retorna string do que foi digitado//
+const txt = document.getElementById("text-input");
 const result = document.getElementById("result");
 
 btn.addEventListener("click", () => {
-  /*Transforma todos os caracters em letra minúscula e substitui os 
- caracters que não são letras de a-z e números de 0-9 por string vazia*/
   const specialChar = txt.value.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+  
   if (txt.value === "") {
     alert("Please input a value");
   } else if (txt.value.length === 1) {
     result.textContent = `${txt.value} é um palíndromo`;
-
-    //Transforma o string em um array, reverte a ordem dos elementos e junta-os.
   } else if (specialChar === specialChar.split("").reverse().join("")) {
     result.textContent = `${txt.value} é um palíndromo`;
   } else {
